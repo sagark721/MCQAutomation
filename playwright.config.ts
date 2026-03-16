a72042ff24ebe333592e1167912589c3a5540843
sagark721 <sagark1431@gmail.com>
2026-03-16 17:49:23 +0530

    updated playwright configuration file and update framework little bit

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,79 +1,91 @@
-import { defineConfig, devices } from '@playwright/test';
+import {defineConfig, devices} from '@playwright/test';
+import dotenv from 'dotenv';
+import path from 'path';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+const testEnv= process.env.TEST_ENV || 'v3';
+
+dotenv.config({ path: path.resolve(`data/env/.env.${testEnv}`)})
+
+dotenv.config({path:path.resolve('.env.local'), override: true})
+
+console.log(`Environment: ${testEnv}`);
+console.log(`Base URL: ${process.env.BASE_URL} \n`)
+
+
 export default defineConfig({
-  testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
+    testDir: './tests',
 
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+    fullyParallel: true,
+
+    workers: process.env.CI ? 4 :2,
+
+    retries: process.env.CI? 2:0,
+
+    timeout: 60 * 1000,
+
+    expect:{
+        timeout:10 * 1000
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+
+    // What to capture on failure
+
+    use:{
+
+        baseURL: process.env.BASE_URL,
+
+        screenshot: 'only-on-failure',
+
+        video: 'retain-on-failure',
+        
+        trace: 'retain-on-failure',
+
+        actionTimeout: 10 * 1000,
+        navigationTimeout: 30 * 1000,
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    // Reports
 
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
+    reporter: [
+        ['html',{outputFolder:'test-results/html', open:'never'}], 
+        ['junit',{outputFile:'test-results/junit/results.xml'}] ,
+        ['list', ]
+    ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
-});
+
+    projects:[
+
+        {
+            name: 'setup',
+            testMatch:'**tests/setup/auth.setup.ts'
+        },
+
+
+        {
+            name:'chromium',
+            use:{
+                ...devices['Desktop Chrome']
+            },
+            dependencies: ['setup'],
+            testIgnore:['**/setup/**']
+        },
+
+        {
+            name: 'android',
+            use:{
+                ...devices['Pixel 7']
+            },
+            dependencies: ['setup'],
+            testIgnore:['**/setup/**']
+        }
+
+
+    ]
+
+
+
+    
+})
