26a2b9b30e5d1fdd0efa03a8bbfc3f99543c8cbe
sagark721 <sagark1431@gmail.com>
2026-03-17 13:41:16 +0530

    Minor changes

---
+++ src/utils/Logger.ts
@@ -0,0 +1,38 @@
+type LogLevel = 'INFO' | 'WARN' | 'ERROR' | 'DEBUG' ;
+
+class Logger{
+
+    private format(level:LogLevel, context:string, message:string): string{
+
+        const timestamp = new Date().toISOString();
+
+        return `[${timestamp}] [${level}] [${context}] ${message}`;
+
+    }
+
+
+
+    info(context:string, message:string):void{
+        console.log(this.format('INFO', context, message));
+    }
+
+
+    warn(context:string, message:string):void{
+        console.warn(this.format('WARN', context, message));
+    }
+
+    error(context:string, message:string):void{
+        console.error(this.format('ERROR',context, message));
+    }
+
+    debug(context:string, message:string):void{
+
+        if (process.env.DEBUG){
+        console.log(this.format('DEBUG', context, message));
+        }
+    }
+
+}
+
+
+export const logger = new Logger();
